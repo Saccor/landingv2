c7aeca5b0a590f796cb9a187c30e35bd35f17940
Saccor <sacco.rezais@gmail.com>
2025-06-10 20:09:14 +0200

    Refactor SurveyPage and SurveyIntroSection components to remove min-height styling, enhancing layout consistency and improving overall design. Adjust padding for better spacing in SurveyIntroSection.

--- src/app/survey/page.tsx
+++ src/app/survey/page.tsx
@@ -2,7 +2,7 @@ import SurveyIntroSection from '@/components/sections/survey/SurveyIntroSection'
 
 export default function SurveyPage() {
   return (
-    <div className="flex flex-col min-h-screen bg-black text-white">
+    <div className="flex flex-col bg-black text-white">
       <SurveyIntroSection />
     </div>
   );
--- src/components/sections/survey/SurveyIntroSection.tsx
+++ src/components/sections/survey/SurveyIntroSection.tsx
@@ -1,7 +1,7 @@
 export default function SurveyPage() {
   return (
-    <div className="flex flex-col min-h-screen bg-black text-white">
-      <div className="flex-grow w-full flex justify-center items-center px-4">
+    <div className="flex flex-col bg-black text-white py-12">
+      <div className="w-full flex justify-center items-center px-4">
         <div className="max-w-[640px] w-full">
           {/* Your content goes here */}
         </div>
