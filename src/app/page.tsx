61562c2e548e08705302a6ea3dbfd44b36207f47
Saccor <sacco.rezais@gmail.com>
2025-06-11 23:59:51 +0200

    Add GrayBoxSection to Home component for enhanced layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import BottomSubscribe from '@/components/sections/BottomSubscribe';
 import SocialMediaSection from '@/components/sections/SocialMediaSection';
 import VisionSection from '@/components/sections/VisionSection';
 import HeroSection from '@/components/sections/HeroSection';
+import GrayBoxSection from '@/components/sections/GrayBoxSection';
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-black">
       <HeroSection />
       <VisionSection />
+      <GrayBoxSection />
       <SocialMediaSection />
       <BottomSubscribe />
     </div>
