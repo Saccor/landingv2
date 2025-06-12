e3609e9fd377eafaa66a958dc4e3c7a845df7dc7
Saccor <sacco.rezais@gmail.com>
2025-06-12 15:03:28 +0200

    Replace GrayBoxSection with FeatureSection in Home component to enhance layout and content presentation.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import BottomSubscribe from '@/components/sections/BottomSubscribe';
 import SocialMediaSection from '@/components/sections/SocialMediaSection';
 import VisionSection from '@/components/sections/VisionSection';
 import HeroSection from '@/components/sections/HeroSection';
-import GrayBoxSection from '@/components/sections/GrayBoxSection';
+import FeatureSection from '@/components/sections/FeatureSection';
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-black">
       <HeroSection />
       <VisionSection />
-      <GrayBoxSection />
+      <FeatureSection />
       <SocialMediaSection />
       <BottomSubscribe />
     </div>
