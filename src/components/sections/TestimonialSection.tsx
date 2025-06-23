b3bce346d109d789cb7273b393ecf67061a5d47b
Saccor <sacco.rezais@gmail.com>
2025-06-23 08:32:45 +0200

    fix: update title in TestimonialSection component for accuracy

--- src/components/sections/TestimonialSection.tsx
+++ src/components/sections/TestimonialSection.tsx
@@ -7,7 +7,7 @@ import RevealSection from '@/components/ui/RevealSection';
 const testimonials = [
   {
     name: 'Marco Andresen',
-    title: 'Chief Operating Officer @ Lentro',
+    title: 'Chief Operating Officer @ Lenovo',
     quote:
       "Driven by a passion for tech and circular economy, I joined Arfve's Advisory Board, inspired by their innovative vision, sustainable model, and dynamic team.",
     image: '/person1carousel.png',
