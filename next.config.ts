08c5b43795be49b5ca572cf9c6d4197d00180ad0
Madhu Sudhan <madhusudhan@arfve.com>
2025-11-16 20:25:50 +0530

    Next config fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   images: {
-    // Remove 'formats' completely
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60 * 60 * 24 * 365,
@@ -16,7 +16,8 @@ const nextConfig = {
     },
   },
 
-  webpack: (config, { isServer }) => {
+  webpack: (config) => {
+    config.resolve = config.resolve || {};
     config.resolve.extensionAlias = {
       '.js': ['.ts', '.tsx', '.js', '.jsx'],
     };
@@ -27,4 +28,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
